fb77878b051b17205c3c54568b6662988dd892f6
MartiresAlbertRick <33756428+MartiresAlbertRick@users.noreply.github.com>
2018-01-22 20:09:58 +0800

    up
    scorecards

---
+++ OPSCO_Web/Controllers/TeamScorecardController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OPSCO_Web.Controllers
+{
+    public class TeamScorecardController : Controller
+    {
+        // GET: TeamScorecard
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
