b84d319978aafc2bd051529f7f65f024e401dd83
Albert Rick G. Martires <martires.albert@outlook.com>
2018-02-08 07:44:55 -0500

    up

---
+++ OPSCO_Web.Tests/Controllers/TeamControllerTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OPSCO_Web.Controllers;
+
+namespace OPSCO_Web.Tests.Controllers
+{
+    [TestClass]
+    public class TeamControllerTest
+    {
+        [TestMethod]
+        public void TestDetailsView()
+        {
+            string test = "Success";
+            Assert.AreEqual("Success", test);
+        }
+    }
+}
